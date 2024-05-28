f7a6dde34978608aab3882ff85c10708397342ae
DRC Lab <85211068+DRCRecoveryData@users.noreply.github.com>
2024-05-28 21:02:31 +0700

    Update RecoveryJpeg.c
    ### Changes and Improvements:
    
    1. **Variable Naming**: Improved variable naming to adhere to common C conventions (`inputFile`, `tempBuffer`, `imageFound`, `imageCount`, etc.).
    
    2. **Error Handling**: Added a check to ensure the last image file is only closed if it was opened.
    
    3. **Comments**: Enhanced comments for better clarity and understanding.
    
    4. **Image File Opening Mode**: Changed the mode for opening new image files from `"a"` (append) to `"wb"` (write binary) to ensure proper file creation and avoid appending to non-existent files.
    
    5. **JPEG Markers Check**: Simplified the JPEG marker check by defining only the necessary markers (`0xe0` and `0xe1`).

--- RecoveryJpeg.c
+++ RecoveryJpeg.c
@@ -1,98 +1,104 @@
-#include <stdio.h> /*standard input output header*/
-#include <stdlib.h> /*standard library for memmory allocation*/
-#include <errno.h> /*system error number */
+#include <stdio.h>  /* Standard input output header */
+#include <stdlib.h> /* Standard library for memory allocation */
+#include <errno.h>  /* System error number */
 
-//main function including command line argument as parameters
-int main(int argc, char ** argv)
+// Main function including command line argument as parameters
+int main(int argc, char **argv)
 {
-    // check if file is input or not
+    // Check if file is input or not
     if (argc == 1)
     {
-        //generating error for file name not found in command line
+        // Generating error for file name not found in command line
         perror("Not entered file name in command line");
-        //returning value which specifies something went wrong
+        // Returning value which specifies something went wrong
         return 1;
     }
-    else
+
+    // Assigning variable for size of JPEG blocks
+    int size = 512;
+
+    // Opening file that is corrupted in read mode
+    FILE *inputFile = fopen(argv[argc - 1], "r");
+
+    // Check if file is empty or cannot be opened
+    if (inputFile == NULL)
     {
-        //assigning variable for size of jpeg blocks
-        int size = 512;
+        // Generating error that file cannot open
+        perror("Can't open raw file");
 
-        //opening file that is corrupted in read mode
-        FILE* InputFile = fopen(argv[argc - 1], "r");
-
-        //check for file is empty or not
-        if (InputFile == NULL)
-        {
-            //generating error that file cannot open
-            perror("Can't open raw file");
-
-            //returning value which implies some error
-            return 2;
-        }
-
-        //defining temprary buffer for storing 512 bytes of data of raw file
-        unsigned char TempBuffer[size];
-
-        //assigning variable to check for image found
-        int ImageFound = 0;
-
-        //assigning variable for number of blocks
-        int block = 1;
-
-        //assigning variable to count images discovered
-        int ImageCount = 0;
-
-        //defining array containing identification of jpeg images to compare with first four bytes of block
-        int jpeg[5] = {0xff, 0xd8, 0xff, 0xe0, 0xe1};
-
-        //assigning file pointer variable for storing image found
-        FILE* image = NULL;
-
-        //while loop to itterate over whole corrupted file to discover image
-        while (fread(TempBuffer, size, block, InputFile))
-        {
-            //check for whether jpeg file is found or not
-            if (TempBuffer[0] == jpeg[0] && TempBuffer[1] == jpeg[1] && TempBuffer[2] == jpeg[2] && (TempBuffer[3] == jpeg[3] || TempBuffer[3] == jpeg[4]))
-            {
-                //check for image is found before or not
-                if (ImageFound)
-                {
-                    //closing image file found
-                    fclose(image);
-                }
-                else
-                {
-                    //assigning value for image found
-                    ImageFound = 1;
-                }
-
-                //character array to specify image file name
-                char fname[10];
-                sprintf(fname, "%04d.jpeg", ImageCount);
-
-                //opening blank image file
-                image = fopen(fname, "a");
-
-                //increasing count of image
-                ImageCount += 1;
-            }
-
-            //check for whether image is found or not
-            if (ImageFound)
-            {
-                //writing the block of size 512 bytes in image file
-                fwrite(&TempBuffer, size, block, image);
-            }
-        }
-
-        //closing last file image found
-        fclose(image);
-
-        //closing input file
-        fclose(InputFile);
-
-        //returning value for successful run of program
-        return 0;
+        // Returning value which implies some error
+        return 2;
     }
+
+    // Defining temporary buffer for storing 512 bytes of data of raw file
+    unsigned char tempBuffer[size];
+
+    // Assigning variable to check for image found
+    int imageFound = 0;
+
+    // Assigning variable for number of blocks
+    int block = 1;
+
+    // Assigning variable to count images discovered
+    int imageCount = 0;
+
+    // Defining array containing identification of JPEG images to compare with first four bytes of block
+    int jpegStart[4] = {0xff, 0xd8, 0xff};
+    int jpegMarker1 = 0xe0;
+    int jpegMarker2 = 0xe1;
+
+    // Assigning file pointer variable for storing image found
+    FILE *image = NULL;
+
+    // While loop to iterate over whole corrupted file to discover image
+    while (fread(tempBuffer, size, block, inputFile))
+    {
+        // Check for whether JPEG file is found or not
+        if (tempBuffer[0] == jpegStart[0] &&
+            tempBuffer[1] == jpegStart[1] &&
+            tempBuffer[2] == jpegStart[2] &&
+            (tempBuffer[3] == jpegMarker1 || tempBuffer[3] == jpegMarker2))
+        {
+            // Check if an image was previously found
+            if (imageFound)
+            {
+                // Closing previous image file found
+                fclose(image);
+            }
+            else
+            {
+                // Assigning value indicating image found
+                imageFound = 1;
+            }
+
+            // Character array to specify image file name
+            char fname[10];
+            sprintf(fname, "%04d.jpeg", imageCount);
+
+            // Opening a new blank image file
+            image = fopen(fname, "wb");
+
+            // Increasing count of image
+            imageCount += 1;
+        }
+
+        // Check if an image has been found
+        if (imageFound)
+        {
+            // Writing the block of size 512 bytes in image file
+            fwrite(tempBuffer, size, block, image);
+        }
+    }
+
+    // Closing last image file found, if any
+    if (image)
+    {
+        fclose(image);
+    }
+
+    // Closing input file
+    fclose(inputFile);
+
+    // Returning value for successful run of program
+    return 0;
 }
